6fd2136ffd6a27a55420f9d6abf7f0f441c41584
Josh Cheek <josh.cheek@gmail.com>
2016-01-03 08:56:15 -0700

    Some abstraction

--- attempt2.rb
+++ attempt2.rb
@@ -1,40 +1,6 @@
 require 'set'
 require 'prime'
 
-class LazyMath
-  def initialize(transformations:[], number: nil)
-    @number          = number
-    @transformations = transformations
-  end
-
-  def *(number)
-    LazyMath.new transformations: @transformations + [[:*, number]]
-  end
-
-  def /(number)
-    LazyMath.new transformations: @transformations + [[:/, number]]
-  end
-
-  def for(number)
-    LazyMath.new number: number, transformations: @transformations
-  end
-
-  def inspect
-    if @number
-      "#{result} = #{@number} #{@transformations.join ' '}"
-    else
-      @transformations.join " "
-    end
-  end
-
-  def result
-    @number or raise "#{inspect} does not have a number, so cannot give a result"
-    @transformations.inject @number do |lhs, (operator, rhs)|
-      lhs.send operator, rhs
-    end
-  end
-end
-
 class Rational
   def to_i!
     raise "#{inspect} is not integral!" if denominator != 1
@@ -63,82 +29,163 @@ module Enumerable
       end
     end.defactor
   end
-
   def defactor
     inject(1) { |product, (num, times)| product * (num**times) }
   end
 end
 
-lower_bound = 32
+
+class LazyMath
+  def initialize(transformations:[], number: nil)
+    @number          = number
+    @transformations = transformations
+  end
+
+  def *(number)
+    LazyMath.new transformations: @transformations + [[:*, number]]
+  end
+
+  def /(number)
+    LazyMath.new transformations: @transformations + [[:/, number]]
+  end
+
+  def for(number)
+    LazyMath.new number: number, transformations: @transformations
+  end
+
+  def inspect
+    if @number
+      "#{call} = #{@number} #{@transformations.join ' '}"
+    else
+      @transformations.join " "
+    end
+  end
+
+  def call
+    @number or raise "#{inspect} does not have a number, so cannot give a result"
+    @transformations.inject @number do |lhs, (operator, rhs)|
+      lhs.send operator, rhs
+    end
+  end
+end
+
+
+class Tuning
+  attr_reader :base_note, :ratios, :lower_bound, :upper_bound
+
+  def initialize(base_note, ratios, lower_bound, upper_bound)
+    @base_note, @ratios, @lower_bound, @upper_bound = base_note, ratios, lower_bound, upper_bound
+  end
+
+  def ratio_denominator
+    @ratio_denominator ||= ratios.lowest_common_denominator
+  end
+
+  def octaves
+    @octaves ||= num_octaves.times.map { |exponent| base_note * (2**exponent) }
+  end
+
+  def notes
+   @notes ||= notes_by_octave.flat_map(&:last)
+  end
+
+  def notes_by_octave
+    @notes_by_octave ||= octaves.map.with_index { |octave, index| [octave, notes_for_octave(octave)] }
+  end
+
+  def notes_per_octave
+    ratios.length
+  end
+
+  def inspect
+    octaves = '%.2f' % (notes.length.to_f/notes_per_octave)
+    "#<Tuning #{base_note}Hz-#{notes.max}Hz, #{notes.length} notes, #{octaves} octaves, #{notes_per_octave} notes / octave>"
+  end
+
+  def partial_octave?
+    notes_per_octave * num_octaves != notes.length
+  end
+
+  def display(header)
+    display = ""
+    display << "=====  #{header}  =====\n"
+    display << "  -----  Stats  -----\n"
+    stats = [ ["#{notes.min} Hz",                             'Lowest note'],
+              ["#{notes.max} Hz",                             'Highest note'],
+              [notes.length,                                  'Notes'],
+              ['%.2f' % (notes.length.to_f/notes_per_octave), 'Octaves'],
+              [notes_per_octave,                              'Notes / Octave']]
+    stat_length = stats.map(&:first).map(&:to_s).map(&:length).max
+    stats.each { |value, description| display << sprintf("  %-#{stat_length}s %s\n", value.to_s, description) }
+    display << "\n"
+    display << "  -----  Octaves  -----\n"
+    normalized_numerator_width = ratios.map { |r| (ratio_denominator * r).to_i!.inspect.length }.max
+    note_width                 = notes.max.inspect.length
+    n_width, d_width           = ratios.map { |r| [r.numerator, r.denominator] }.transpose.map { |ns| ns.map(&:inspect).map(&:length).max }
+    octave_index_length        = notes_by_octave.length.pred.to_s.length
+
+    notes_by_octave.each_with_index do |(octave, notes), octave_index|
+      notes.each_with_index do |note, note_index|
+        format_string = "  %#{octave_index_length+1}s "
+        format_string %= (note_index.zero? ?  "#{octave_index}:" : "")
+        format_string += "%-#{note_width+2}s | %#{normalized_numerator_width}d/#{ratio_denominator} | %#{n_width}d / %#{d_width}d | %p\n"
+        ratio = note.to_r / octave
+        display << sprintf(format_string, "#{note}Hz", (ratio_denominator*ratio).to_i!, ratio.numerator, ratio.denominator, ratio.to_math.for(octave))
+      end
+    end
+    display
+  end
+
+  private
+
+  def num_octaves
+    @num_octaves ||= Math.log2(upper_bound.to_f/base_note).to_i.next
+  end
+
+  def notes_for_octave(octave)
+    ratios.map { |ratio| ratio.to_math.for(octave).call }
+          .select { |note| lower_bound <= note && note <= upper_bound }
+  end
+end
+
+# ====================================================
+
+lower_bound = 2
 upper_bound = 4400
 
-notes = Set[1]
-[2, 3, 5].each do |multiplier|
-  to_multiply = notes.to_a
+potential_notes = Set[1]
+[2, 3, 5, 7].each do |multiplier|
+  to_multiply = potential_notes.to_a
   while to_multiply.any?
     next_to_multiply = []
     to_multiply.each do |note|
       note *= multiplier
       next unless note <= upper_bound
-      notes << note
+      potential_notes << note
       next_to_multiply << note
     end
     to_multiply = next_to_multiply
   end
 end
-notes.reject! { |note| note < lower_bound }
+potential_notes.reject! { |note| note < lower_bound }
 
-notes.length # => 121
-notes_to_ratios = notes
-                    .map do |note1|
-                      [ note1,
-                        notes.select { |note2| note1 < note2 }
-                             .select { |note2| note2 < 2*note1 }
-                             .map    { |note2| note2.to_r / note1 }
-                             .reject { |ratio| ratio == 1 }
-                             .sort
-                      ]
-                    end
-                    .reject do |note, ratios|
-                      ratios.length < 12
-                    end
-                    .sort_by do |note1, ratios|
-                      ratios.inject(1) { |multiplier, ratio| multiplier * ratio.numerator }
-                    end
+notes_to_ratios = potential_notes.map do |note1|
+  [ note1,
+    potential_notes.select { |note2| note1 <= note2 }
+    .select { |note2| note2 < 2*note1 }
+    .map    { |note2| note2.to_r / note1 }
+    .sort
+  ]
+end
 
-base_note, ratios = notes_to_ratios.first
-base_note # => 120
-ratios    # => [(25/24), (16/15), (9/8), (6/5), (5/4), (4/3), (27/20), (3/2), (8/5), (5/3), (9/5), (15/8)]
-  .length # => 12
+tunings = notes_to_ratios.map { |base_note, ratios| Tuning.new base_note, ratios, lower_bound, upper_bound }
 
-ratios.lowest_common_denominator # => 120
-      .prime_factors             # => [2, 2, 2, 3, 5]
+# Potentials
+tunings.length # => 252
+tunings.map { |t| t.notes.length }.sort_by { |l| (l-88).abs }.take(10)     # => [88, 88, 89, 89, 87, 87, 87, 86, 86, 90]
+tunings.map { |t| t.notes_per_octave }.sort_by { |l| (l-12).abs }.take(10) # => [12, 12, 12, 12, 12, 13, 11, 11, 13, 13]
 
-ratios.map { |ratio| [ratio, ratio.to_math.for(base_note)] }
-# => [[(25/24), 125 = 120 * 5 * 5 / 2 / 2 / 2 / 3],
-#     [(16/15), 128 = 120 * 2 * 2 * 2 * 2 / 3 / 5],
-#     [(9/8), 135 = 120 * 3 * 3 / 2 / 2 / 2],
-#     [(6/5), 144 = 120 * 2 * 3 / 5],
-#     [(5/4), 150 = 120 * 5 / 2 / 2],
-#     [(4/3), 160 = 120 * 2 * 2 / 3],
-#     [(27/20), 162 = 120 * 3 * 3 * 3 / 2 / 2 / 5],
-#     [(3/2), 180 = 120 * 3 / 2],
-#     [(8/5), 192 = 120 * 2 * 2 * 2 / 5],
-#     [(5/3), 200 = 120 * 5 / 3],
-#     [(9/5), 216 = 120 * 3 * 3 / 5],
-#     [(15/8), 225 = 120 * 3 * 5 / 2 / 2 / 2]]
+tuning = tunings.sort_by { |t| (t.notes.length-88).abs + (t.notes_per_octave-12).abs }.first
+tuning # => #<Tuning 27Hz-4096Hz, 88 notes, 7.33 octaves, 12 notes / octave>
 
-octave = base_note * 2
-ratios.map { |ratio| [ratio, ratio.to_math.for(octave)] }
-# => [[(25/24), 250 = 240 * 5 * 5 / 2 / 2 / 2 / 3],
-#     [(16/15), 256 = 240 * 2 * 2 * 2 * 2 / 3 / 5],
-#     [(9/8), 270 = 240 * 3 * 3 / 2 / 2 / 2],
-#     [(6/5), 288 = 240 * 2 * 3 / 5],
-#     [(5/4), 300 = 240 * 5 / 2 / 2],
-#     [(4/3), 320 = 240 * 2 * 2 / 3],
-#     [(27/20), 324 = 240 * 3 * 3 * 3 / 2 / 2 / 5],
-#     [(3/2), 360 = 240 * 3 / 2],
-#     [(8/5), 384 = 240 * 2 * 2 * 2 / 5],
-#     [(5/3), 400 = 240 * 5 / 3],
-#     [(9/5), 432 = 240 * 3 * 3 / 5],
-#     [(15/8), 450 = 240 * 3 * 5 / 2 / 2 / 2]]
+puts tuning.display("Best piano fit")
