b238cd774464f4be66348d578712c1184a738f78
Josh Cheek <josh.cheek@gmail.com>
2016-01-03 08:59:53 -0700

    Extract copy/pasted code into a separate file

---
+++ common.rb
@@ -0,0 +1,232 @@
+require 'set'
+require 'prime'
+
+class LazyMath
+  def initialize(transformations:[], number: nil)
+    @number          = number
+    @transformations = transformations
+  end
+
+  def *(number)
+    LazyMath.new transformations: @transformations + [[:*, number]]
+  end
+
+  def /(number)
+    LazyMath.new transformations: @transformations + [[:/, number]]
+  end
+
+  def for(number)
+    LazyMath.new number: number, transformations: @transformations
+  end
+
+  def inspect
+    if @number
+      "#{call} = #{@number} #{@transformations.join ' '}"
+    else
+      @transformations.join " "
+    end
+  end
+
+  def call
+    @number or raise "#{inspect} does not have a number, so cannot give a result"
+    @transformations.inject @number do |lhs, (operator, rhs)|
+      lhs.send operator, rhs
+    end
+  end
+end
+
+
+class Human
+  attr_reader :rational
+  protected :rational
+
+  def initialize(rational)
+    @rational = rational
+  end
+
+  def to_i
+    rational.to_i
+  end
+
+  def integer?
+    rational == to_i
+  end
+
+  def inspect
+    return to_i.to_s if integer?
+    "(#{to_i}+#{remainder})"
+  end
+
+  def to_human
+    self
+  end
+
+  def remainder
+    return 0 if integer?
+    rational - to_i
+  end
+
+  include Comparable
+  def <=>(other)
+    rational <=> other.to_human.rational
+  end
+end
+
+
+class Rational
+  def to_i!
+    raise "#{inspect} is not integral!" if denominator != 1
+    numerator
+  end
+
+  def to_human
+    Human.new self
+  end
+
+  def to_math
+    math = LazyMath.new
+    numerator.prime_factors.each   { |factor| math *= factor }
+    denominator.prime_factors.each { |factor| math /= factor }
+    math
+  end
+end
+
+
+class Integer
+  def prime_factors
+    prime_division.flat_map { |prime, times| Array.new times, prime }
+  end
+
+  def to_human
+    Human.new self
+  end
+end
+
+
+module Enumerable
+  def lowest_common_denominator
+    map(&:denominator).prime_union
+  end
+
+  def lowest_common_multiple
+    denominator = lowest_common_denominator
+    numerator   = map { |rational| (rational * denominator).numerator }.prime_union
+    numerator.to_r / denominator
+  end
+
+  def prime_union
+    each_with_object Hash.new do |integer, factors|
+      integer.prime_division.each do |num, times|
+        factors[num] ||= times
+        factors[num] = times if factors[num] < times
+      end
+    end.defactor
+  end
+
+  def prime_intersection
+    each_with_object(prime_union.prime_division.to_h) do |integer, all_factors|
+      current_factors = integer.prime_division.to_h
+      all_factors.select! { |prime, exponent| current_factors.key? prime }
+      current_factors.each do |prime, exponent|
+        next unless all_factors.key? prime
+        next unless exponent < all_factors[prime]
+        all_factors[prime] = exponent
+      end
+    end.defactor
+  end
+
+  def defactor
+    inject(1) { |product, (num, times)| product * (num**times) }
+  end
+
+  def dissonance
+    # multiplying by denominator b/c, eg 4/3 will need to repeat 3 times
+    # before it falls on an integral boundary
+    ints = map { |n| n.numerator * n.denominator }
+    dissonance = ints.prime_union / ints.prime_intersection
+    if dissonance.denominator == 1
+      dissonance.numerator
+    else
+      dissonance
+    end
+  end
+end
+
+
+class Tuning
+  attr_reader :base_note, :ratios, :lower_bound, :upper_bound
+
+  def initialize(base_note, ratios, lower_bound, upper_bound)
+    @base_note, @ratios, @lower_bound, @upper_bound = base_note, ratios, lower_bound, upper_bound
+  end
+
+  def ratio_denominator
+    @ratio_denominator ||= ratios.lowest_common_denominator
+  end
+
+  def octaves
+    @octaves ||= num_octaves.times.map { |exponent| base_note * (2**exponent) }
+  end
+
+  def notes
+   @notes ||= notes_by_octave.flat_map(&:last)
+  end
+
+  def notes_by_octave
+    @notes_by_octave ||= octaves.map.with_index { |octave, index| [octave, notes_for_octave(octave)] }
+  end
+
+  def notes_per_octave
+    ratios.length
+  end
+
+  def inspect
+    octaves = '%.2f' % (notes.length.to_f/notes_per_octave)
+    "#<Tuning #{base_note}Hz-#{notes.max}Hz, #{notes.length} notes, #{octaves} octaves, #{notes_per_octave} notes / octave>"
+  end
+
+  def partial_octave?
+    notes_per_octave * num_octaves != notes.length
+  end
+
+  def display(header)
+    display = ""
+    display << "=====  #{header}  =====\n"
+    display << "  -----  Stats  -----\n"
+    stats = [ ["#{notes.min} Hz",                             'Lowest note'],
+              ["#{notes.max} Hz",                             'Highest note'],
+              [notes.length,                                  'Notes'],
+              ['%.2f' % (notes.length.to_f/notes_per_octave), 'Octaves'],
+              [notes_per_octave,                              'Notes / Octave']]
+    stat_length = stats.map(&:first).map(&:to_s).map(&:length).max
+    stats.each { |value, description| display << sprintf("  %-#{stat_length}s %s\n", value.to_s, description) }
+    display << "\n"
+    display << "  -----  Octaves  -----\n"
+    normalized_numerator_width = ratios.map { |r| (ratio_denominator * r).to_i!.inspect.length }.max
+    note_width                 = notes.max.inspect.length
+    n_width, d_width           = ratios.map { |r| [r.numerator, r.denominator] }.transpose.map { |ns| ns.map(&:inspect).map(&:length).max }
+    octave_index_length        = notes_by_octave.length.pred.to_s.length
+
+    notes_by_octave.each_with_index do |(octave, notes), octave_index|
+      notes.each_with_index do |note, note_index|
+        format_string = "  %#{octave_index_length+1}s "
+        format_string %= (note_index.zero? ?  "#{octave_index}:" : "")
+        format_string += "%-#{note_width+2}s | %#{normalized_numerator_width}d/#{ratio_denominator} | %#{n_width}d / %#{d_width}d | %p\n"
+        ratio = note.to_r / octave
+        display << sprintf(format_string, "#{note}Hz", (ratio_denominator*ratio).to_i!, ratio.numerator, ratio.denominator, ratio.to_math.for(octave))
+      end
+    end
+    display
+  end
+
+  private
+
+  def num_octaves
+    @num_octaves ||= Math.log2(upper_bound.to_f/base_note).to_i.next
+  end
+
+  def notes_for_octave(octave)
+    ratios.map { |ratio| ratio.to_math.for(octave).call }
+          .select { |note| lower_bound <= note && note <= upper_bound }
+  end
+end
